458f8145981681a95611b5d6d63adeb8da0f7d55
maciek.glowka <maciek.glowka@gmail.com>
2025-04-15 22:45:26 +0200

    scheduler epochs

--- src/scheduler.rs
+++ src/scheduler.rs
@@ -17,7 +17,7 @@ impl<W: 'static> Scheduler<W> {
             sender: Sender::new(),
         }
     }
-    pub fn subscribe<T: 'static, M>(&mut self, handler: impl IntoHandler<T, W, M>) {
+    pub fn add_system<T: 'static, M>(&mut self, handler: impl IntoHandler<T, W, M>) {
         self.handlers
             .entry(TypeId::of::<T>())
             .or_insert(HandlerSet(Vec::new()))
@@ -34,6 +34,9 @@ impl<W: 'static> Scheduler<W> {
                     set.handle(&mut event.1, world, &mut self.sender);
                 }
             }
+        }
+        if !self.sender.0.is_empty() {
+            self.queue.push_back(self.sender.0.drain(..).collect());
         }
     }
 }
@@ -183,7 +186,7 @@ mod tests {
         }
 
         let mut scheduler = Scheduler::new();
-        scheduler.subscribe(attack_handler);
+        scheduler.add_system(attack_handler);
 
         let mut world = World;
         scheduler.send(Attack(13));
@@ -202,7 +205,7 @@ mod tests {
         }
 
         let mut scheduler = Scheduler::new();
-        scheduler.subscribe(attack_handler);
+        scheduler.add_system(attack_handler);
 
         let mut world = World(0);
         scheduler.send(Attack(13));
@@ -220,15 +223,16 @@ mod tests {
         }
 
         let mut scheduler = Scheduler::new();
-        scheduler.subscribe(attack_handler);
+        scheduler.add_system(attack_handler);
 
         let mut world = World;
         scheduler.send(Attack(13));
         scheduler.step(&mut world);
         assert_eq!(
             scheduler
-                .sender
-                .0
+                .queue
+                .get(0)
+                .unwrap()
                 .get(0)
                 .unwrap()
                 .1
@@ -250,15 +254,16 @@ mod tests {
         }
 
         let mut scheduler = Scheduler::new();
-        scheduler.subscribe(attack_handler);
+        scheduler.add_system(attack_handler);
 
         let mut world = World(0);
         scheduler.send(Attack(13));
         scheduler.step(&mut world);
         assert_eq!(
             scheduler
-                .sender
-                .0
+                .queue
+                .get(0)
+                .unwrap()
                 .get(0)
                 .unwrap()
                 .1
@@ -268,5 +273,54 @@ mod tests {
             30
         );
         assert_eq!(13, world.0)
+    }
+    #[test]
+    fn test_handler_epochs() {
+        // Event.
+        struct Attack(u32);
+        struct World(u32);
+
+        fn attack_handler(attack: &mut Attack, world: &mut World, sender: &mut Sender) {
+            world.0 += attack.0;
+            sender.send(Attack(attack.0));
+        }
+
+        let mut scheduler = Scheduler::new();
+        scheduler.add_system(attack_handler);
+
+        let mut world = World(0);
+        scheduler.send(Attack(1));
+        for _ in 0..5 {
+            scheduler.step(&mut world);
+        }
+        assert_eq!(5, world.0)
+    }
+    #[test]
+    fn test_resulting_event() {
+        // Events.
+        struct Attack(u32);
+        struct Damage(u32);
+
+        struct World(u32);
+
+        fn attack_handler(attack: &mut Attack, sender: &mut Sender) {
+            sender.send(Damage(2 * attack.0));
+        }
+
+        fn damage_handler(damage: &mut Damage, world: &mut World) {
+            world.0 = damage.0;
+        }
+
+        let mut scheduler = Scheduler::new();
+        scheduler.add_system(attack_handler);
+        scheduler.add_system(damage_handler);
+
+        let mut world = World(0);
+        scheduler.send(Attack(3));
+
+        for _ in 0..2 {
+            scheduler.step(&mut world);
+        }
+        assert_eq!(6, world.0)
     }
 }
